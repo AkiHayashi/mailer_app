9dc7c6d60c624d79f61e952e2353ad5ffd2d7f7a
aki hayashi <hayashi1019toy@gmail.com>
2021-08-16 12:25:19 +0900

    変更ファイルはないが念のため再コミット

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 class ContactMailer < ApplicationMailer
   def contact_mail(contact)
     @contact = contact
-    mail to: "hayashiaki.13@gmail.com", subject: "お問い合わせの確認メール"
+    mail to: "hayashiaki.13@icloud.com", subject: "お問い合わせの確認メール"
   end
 end
